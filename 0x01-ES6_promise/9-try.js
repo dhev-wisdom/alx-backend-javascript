e7c04e7044600944550a2b218c72684a56ae178c
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-26 17:32:59 +0000

    Check

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(err);
+    queue.push(err.message);
   } finally {
     queue.push('Guardrail was processed');
   }
