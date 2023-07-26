ef82cb782c8e2ba05cec3866336bd46a26bc0257
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-26 07:59:58 +0000

    Fix Promise return from function | and function export

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,3 @@
-const getResponseFromAPI = Promise.resolve();
-/*{
-  const promiseObj = new Promise((resolve, reject) => {
-    const x = 2;
-    if (x > 0) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
-
-  return promiseObj;
-  // return Promise.resolve();
-}*/
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else if (success === false) {
-      reject (new Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
