92ec7bffaaa4dc5b906758ddd57072ad30424f03
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-26 23:47:50 +0000

    Check

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(), uploadPhoto()])
-  .then((value) => {
-    console.log(value);
-    return [{ status: value.status, value}];
-  })
-    .catch((err) => err);
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const arr = [];
+      let obj = {};
+      values.forEach((value) => {
+        obj = { status: value.status, value };
+        arr.append(obj);
+      });
+
+      return arr;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
