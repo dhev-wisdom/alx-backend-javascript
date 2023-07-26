d2aa468106a73b244be95894d8296ec44634738c
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-26 07:54:36 +0000

    check

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({'status': 200, 'body': 'Success'});
+      resolve({status: 200, body: 'Success'});
     } else if (success === false) {
-      reject('The fake API is not working currently');
+      reject (new Error('The fake API is not working currently'));
     }
   });
 }
