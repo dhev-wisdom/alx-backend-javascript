955fe5dad6b0d592ebc25c3e54ef3c45e1fdec12
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-26 22:41:24 +0000

    check

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,20 @@
-export default function handleResponseFromAPI(promise) {
+/*export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
   })
     .catch((err) => err);
+}*/
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return { status: 200, body: 'success'}
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
