deee0a3372887c102e09b873a30376470b3a834d
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-27 23:59:07 +0000

    Check: super()

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super();
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new Error('Amount must be a number');
+    }
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new Error('Currency must be of type Currency (class)');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new Error('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new Error('Currency must be of type Currency (class)');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code}}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
