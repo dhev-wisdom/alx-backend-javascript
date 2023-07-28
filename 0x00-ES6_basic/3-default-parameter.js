7efdb4f23c6cfe0978131e9fb76ab27ddebc3e61
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-28 19:53:19 +0000

    default parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,12 +1,3 @@
-export default function getSumOfHoods(
-  initialNumber, expansion1989 = undefined, expansion2019 = undefined,
-) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
