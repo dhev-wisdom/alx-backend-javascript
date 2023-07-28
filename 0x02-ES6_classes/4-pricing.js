0fc06091a6ce0b492c89e90539c4be8d4b602354
dhev-wisdom <nonsowisdom62@gmail.com>
2023-07-28 01:55:41 +0000

    Implemet Abstract-class-like behaviour

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,7 +2,7 @@ import Currency from './3-currency';
 
 export default class Pricing extends Currency {
   constructor(amount, currency) {
-    super();
+
     if (typeof amount === 'number') {
       this._amount = amount;
     } else {
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+}
