0868fd06ec965821886c116ef317c85dfe700228
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-19 02:40:17 +0100

    arg

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const read = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
 read.question("Welcome to Holberton School, what is your name?\n", (name) => {
   console.log(`Your name is: ${name}`);
 });
 
-process.on('exit', function(arg) {
-  console.log('This important software is closing')
-})
+process.on('exit', function() {
+  console.log('This important software is now closing')
+}); 
