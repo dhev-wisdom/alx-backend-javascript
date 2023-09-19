d7be3f347f52457d3b48c9dbf3abdecc203a4a4c
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-19 17:16:41 +0100

    check

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const countStudents = (path) => (
+  new Promise((resolve, reject) => {
+    try {
+      fs.readFile(path, 'utf-8', ((err, data) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+        const studentsCount = {};
+        const lines = data.split('\n');
+        console.log(`Number of students: ${lines.length - 2}`);
+
+        for (let i = 1; i < lines.length - 1; i += 1) {
+          const line = lines[i];
+          const parts = line.split(',');
+          const field = parts[parts.length - 1].trim();
+          if (!studentsCount[field] && field !== 'field' && field !== '') {
+            studentsCount[field] = [];
+          }
+          studentsCount[field].push(parts[0].trim());
+        }
+
+        Object.keys(studentsCount).forEach((key) => {
+          const values = studentsCount[key];
+          console.log(`Number of students in ${key}: ${values.length}. List: ${values.join(', ')}`);
+        });
+        resolve();
+      }));
+    } catch (err) {
+      reject(new Error('Cannot load the database'));
+    }
+  })
+);
+
+module.exports = countStudents;
