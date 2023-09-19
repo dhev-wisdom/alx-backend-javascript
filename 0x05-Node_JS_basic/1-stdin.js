99ed5809a6ce15fa23fe7c59ff395dfb52264ee4
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-19 13:12:47 +0100

    Check

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,10 +5,10 @@ const read = readline.createInterface({
   output: process.stdout,
 });
 
-read.question("Welcome to Holberton School, what is your name?\n", (name) => {
+read.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
 });
 
-process.on('exit', function() {
-  console.log('This important software is now closing')
-}); 
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
