cdedaccd20a114c516ff8a7215b5398d8560fbfe
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-20 12:24:04 +0100

    res.write; res.end

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const students = await countStudents(process.argv[2]);
+    res.write('This is the list of our students\n');
+    res.write(students.join('\n'));
+    res.end();
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
