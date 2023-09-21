f041d920a4aee9fd627539b21c914707ca3d0760
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-21 22:21:49 +0100

    check

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,12 +1,5 @@
 const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') return Math.round(a) + Math.round(b);
-  else if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
-  else if (type === 'DIVIDE') {
-    if (Math.round(b) === 0) {
-      return 'Error';
-    }
-    return Math.round(a) / Math.round(b)
-  }
+  return Math.round(a) / Math.round(b);
 }
 
 module.exports = calculateNumber; 
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,19 +4,19 @@ const assert = require('assert');
 
 describe('calculateNumber', function() {
   it('Should return the sum of two params', function() {
-    const result = calculateNumber('SUM', 1.4, 4.5);
+    const result = calculateNumber(1, 3);
+    assert.equal(result, 4);
+  });
+  it('Should return the sum of two params', function() {
+    const result = calculateNumber(1, 3.7);
+    assert.equal(result, 5);
+  });
+  it('Should return the sum of two params', function() {
+    const result = caalculateNumber(1.2, 3.7);
+    assert.equal(result, 5);
+  });
+  it('Should return the sum of two params', function() {
+    const result = calculateNumber(1.5, 3.7);
     assert.equal(result, 6);
-  });
-  it('Should return the value of param_1 minus param_2', function() {
-    const result = calculateNumber('SUBTRACT', 1.4, 4.5);
-    assert.equal(result, -4);
-  });
-  it('Should return the value of param_1 divided by param_2', function() {
-    const result = calculateNumber('DIVIDE', 1.4, 4.5);
-    assert.equal(result, 0.2);
-  });
-  it('Should return "Error" if divisor is 0', function() {
-    const result = calculateNumber('DIVIDE', 1.4, 0);
-    assert.equal(result, 'Error');
   });
 });
