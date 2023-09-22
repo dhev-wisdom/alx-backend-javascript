8d8a622d3f2a65e94e658500bc2688965727bcc7
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-22 09:51:04 +0100

    check

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -8,6 +8,19 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id', validateIdParam, (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
-})
+});
+
+function validateIdParam(req, res, next) {
+  const id = req.params.id;
+  if (!/^\d+$/.test(id)) {
+    return res.status(404).send(`Cannot GET /cart/${id}. id must be a number.\nn`);
+  }
+  next();
+}
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,14 +1,24 @@
-const { default: axios } = require('axios');
+const request = require('request');
 
 const { expect } = require('chai');
 
 describe('API test', function() {
-  it('it should return statusCode of 200 for GET 127.0.0.1:7865/', async function() {
-    const response = await axios.get('http://localhost:7865');
-    expect(response.status).to.equal(200);
+  it('it should return statusCode of 200 for GET 127.0.0.1:7865/cart/<id:int>', function(done) {
+    request('http://localhost:7865/cart/12', (err, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
-  it('it should return body of "Welcom..." for GET 127.0.0.1:7865/', async function() {
-    const response = await axios.get('http://localhost:7865');
-    expect(response.data).to.equal('Welcome to the payment system');
+  it('it should return correct data for GET 127.0.0.1:7865/cart/<id:int>', function(done) {
+    request('http://localhost:7865/cart/12', (err, response, body) => {
+      expect(body).to.equal(`Payment methods for cart 12`);
+      done();
+    });
+  });
+  it('it should return statusCode of 404 for GET 127.0.0.1:7865/cart/<id:str>', function(done) {
+      request('http://localhost:7865/cart/hello', (err, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
   });
 });
