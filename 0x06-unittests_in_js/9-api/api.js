2df4184255c49eef85b0546306864c61452bcaa5
dhev-wisdom <nonsowisdom62@gmail.com>
2023-10-27 00:13:24 +0100

    check 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', validateIdParam, (req, res) => {
+app.get('/cart/:id([0-9]+)', (req, res) => {
   const id = req.params.id;
   res.send(`Payment methods for cart ${id}`);
 });
